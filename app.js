12059168017df500858a3dd64221961a4269f575
Candgrmc <candgrmc@gmail.com>
2022-08-06 15:59:35 +0300

    .

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const GH = require('./lib/gh');
+const EB = require('./lib/eb');
+const Zip = new (require('./lib/zip'))();
+
+const repositoryAppMap = require('./repositoryAppMap');
+
+
 var port = process.env.PORT || 3000,
     http = require('http'),
     fs = require('fs'),
@@ -6,6 +13,7 @@ var port = process.env.PORT || 3000,
 var log = function(entry) {
     fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
 };
+log = console.log;
 
 var server = http.createServer(function (req, res) {
     if (req.method === 'POST') {
@@ -17,7 +25,31 @@ var server = http.createServer(function (req, res) {
 
         req.on('end', function() {
             if (req.url === '/') {
-                log('Received message: ' + body);
+                const bodyAsJson = JSON.parse(body);
+                const gh = new GH(bodyAsJson.repository);
+
+                gh.clone().then(result => {
+                    Zip.zipCloned().then(({path,label})=> {
+                        console.log('path',path);
+                        console.log('label',label);
+
+                        console.log('#########################################################');
+                        console.log('deploying...');
+                        console.log('#########################################################');
+                        log(result);
+                        const eb = new EB({
+                            app_name: repositoryAppMap[bodyAsJson.repository.name],
+                            target_env: repositoryAppMap[bodyAsJson.repository.name] + '-dev',
+                            bucket: 'elasticbeanstalk-eu-west-1-236019483442',
+                        });
+
+                        eb.deploy(path,label).then(r => {
+                            console.log('deployed..');
+                        })
+                        
+                    })
+                })
+                //log('Received message: ' + body);
             } else if (req.url = '/scheduled') {
                 log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
             }
